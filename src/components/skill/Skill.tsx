1bc99f9ca3c3306a34fc148c3a3ce29778dd6b04
shrish0731 <shrish.karki@soanitech.com>
2024-12-12 22:06:55 +0545

    learning/ queryBy

--- src/components/skill/Skill.tsx
+++ src/components/skill/Skill.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { SkillsProps } from "./Skills.types";
 
 const Skill = (props: SkillsProps) => {
+    const [isLogin,setIslogin]=useState<boolean>(false)
   const { skills } = props;
 
   return (
@@ -10,6 +12,9 @@ const Skill = (props: SkillsProps) => {
           return <li key={index}>{skill}</li>;
         })}
       </ul>
+      {
+        isLogin ?(<button>Start Learning</button>):(<button onClick={()=>setIslogin(true)}>Login</button>)
+      }
     </>
   );
 };
